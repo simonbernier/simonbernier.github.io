a46ea2c1a2c20566367cc1bde531add6b2367776
simonbernier <37701567+simonbernier@users.noreply.github.com>
2025-10-09 01:27:30 +0000

    Deploying to gh-pages from @ simonbernier/simonbernier.github.io@ca531517463cd2c063635bdc2e004ddff0456cf3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,8 +3,8 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-about",
-    title: "about",
+    id: "nav-à-propos",
+    title: "À propos",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
